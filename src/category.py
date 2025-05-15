99a02953343cdf2c0ad4a7b0bdc1e77021c0cc6f
Dmitriy Davydov <koldoond@gmail.com>
2025-05-15 13:32:07 +0400

    \123

--- src/category.py
+++ src/category.py
@@ -11,17 +11,3 @@ class Category:
         self.products = products
         Category.category_count += 1
         Category.product_count += len(products)
-
-    def add_product(self, product):
-        """Добавление объекта в приватный атрибут __products"""
-        self.__products.append(product)
-        Category.product_count += 1
-
-    @property
-    def products(self):
-        """Возвращение информации по товару в виде строки:
-        Название продукта, _ руб. Остаток: _ шт."""
-        products_str = ""
-        for product in self.__products:
-            products_str += f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт.\n"
-        return products_str
