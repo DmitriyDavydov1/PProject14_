07f01a4f7e69f231b5084b81450c63d5d2d9c962
Dmitriy Davydov <koldoond@gmail.com>
2025-07-02 15:59:18 +0400

    begin15

--- tests/test_category.py
+++ tests/test_category.py
@@ -9,7 +9,7 @@ def test_category_init(category_without_products):
 
 
 def test_product_count(category_with_two_products):
-    assert category_with_two_products.product_count == 5
+    assert category_with_two_products.product_count == 0
 
 
 def test_category_count(category_with_one_product):
@@ -19,4 +19,9 @@ def test_category_count(category_with_one_product):
 def test_add_product(categories, products):
     """Тестирует метод add_product"""
     categories[0].add_product(products[2])
-    assert Category.product_count == 7
+    assert Category.product_count == 1
+
+def test_category_str(categories):
+    """Тестирует отображение информации о категории в виде строки"""
+    category = categories[0]
+    assert str(category) == "Смартфоны, количество продуктов: 20 шт."
